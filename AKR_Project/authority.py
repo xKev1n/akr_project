705f8cafe72668f2d271c37943def3abb64dfa64
peny753 <60744158+peny753@users.noreply.github.com>
2021-11-29 19:57:43 +0100

    One_of_last_commits_HOPE_SO

--- AKR_Project/authority.py
+++ AKR_Project/authority.py
@@ -1,35 +1,36 @@
 from hashlib import sha512
-from functions import GenerateKeyPair
 import time
 
+
 class Authority():
-	def __init__(self, name):
-		self.name = name
-		self.keyPair = GenerateKeyPair(self.name)
 
-	def GenerateCertificate(self, e, entity):
-		if entity.keyPair.e == e:	
-			hash = int.from_bytes(sha512(bytes(e)).digest(), byteorder='big')
-			
-			certificate_hash = hex(pow(hash, self.keyPair.d, self.keyPair.n))
-			
-			owner = entity.name
-			validFrom = time.time()
-			validTo = validFrom + 31556926			# Certificate is valid for one year
-			algorithm = "RSA"
-			public_key = hex(e)
-			from_authority = self.name
-			
+    def __init__(self, name):
+        self.name = name
+        self.id = "Authority"
+        self.keyPair = None
 
-			certificate = {
-			"Owner": owner,
-			"ValidFrom": validFrom,
-			"ValidTo": validTo,
-			"Algorithm": algorithm,
-			"Public_Key": public_key,
-			"From_Authority": from_authority,
-			"Certificate": certificate_hash
-			}
+    def GenerateCertificate(self, e, entity):
+        if entity.keyPair.e == e:
+            hash = int.from_bytes(sha512(bytes(e)).digest(), byteorder='big')
 
-			return certificate
-	
+            certificate_hash = hex(pow(hash, self.keyPair.d, self.keyPair.n))
+
+            owner = entity.name
+            validFrom = time.time()
+            validTo = validFrom + 31556926  # Certificate is valid for one year
+            algorithm = "RSA"
+            public_key = hex(e)
+            from_authority = self.name
+
+            certificate = {
+                "Owner": owner,
+                "ValidFrom": validFrom,
+                "ValidTo": validTo,
+                "Algorithm": algorithm,
+                "Public_Key": public_key,
+                "From_Authority": from_authority,
+                "Certificate": certificate_hash
+            }
+
+            return certificate
+
--- AKR_Project/main.py
+++ AKR_Project/main.py
@@ -1,83 +1,81 @@
-from functions import VerifyCertificate, VerifySignature, get_keypair_from_file
+import json
+
+from Crypto.PublicKey import RSA
+from functions import GenerateKeyPair, VerifyCertificate, VerifySignature, save_keypair_to_file
 from entity import Entity
 from authority import Authority
+import apk
+import os
 
 
-# Entities
-entity1 = Entity("Entity1")
-# creating new entity
+def initialize_entities_authorities():
+	with open("keypairs.json" ,"r") as f:
+		data = json.load(f)
+		theArray = data["objects"]
+		for item in theArray:
+			name = item["name"]
+			key = item["key"]
+			bitKey = key.encode('UTF-8')
+			finKey = RSA.importKey(bitKey)
 
-# testing saving and retrieving keypair from file
-keypairE1 = get_keypair_from_file(entity1.name)
+			if item["id"] == "Authority":
+				authDic[name] = Authority(name)
+				authDic[name].keyPair = finKey
 
-print("Entity 1")
-print(f"Public key:  (e={hex(keypairE1.e)})")
-print(f"Private key: (d={hex(keypairE1.d)})")
-print(f"Modulo: (n={hex(keypairE1.n)})")
-print(hex(keypairE1.e) == hex(entity1.keyPair.e))
-print(hex(keypairE1.d) == hex(entity1.keyPair.d))
-print(hex(keypairE1.n) == hex(entity1.keyPair.n))
-
-
-entity2 = Entity("Entity2")
-# creating new entity
-
-keypairE2 = get_keypair_from_file(entity2.name)
-
-print("Entity2")
-print(f"Public key:  (e={hex(keypairE2.e)})")
-print(f"Private key: (d={hex(keypairE2.d)})")
-print(f"Modulo: (n={hex(keypairE2.n)})")
-print(hex(keypairE2.e) == hex(entity2.keyPair.e))
-print(hex(keypairE2.d) == hex(entity2.keyPair.d))
-print(hex(keypairE2.n) == hex(entity2.keyPair.n))
+			elif item["id"] == "Entity":
+				entDic[name] = Entity(name)
+				entDic[name].keyPair = finKey
 
 
-# Authorities
-authority1 = Authority("CZ_AUTHORITY")
-# creating new authority
+def signEval():
+	filePath = apk.checkFile.cget("text")
+	ent = os.path.basename(apk.checkFile.cget("text"))
+	print(ent)
+	print(filePath)
 
-sigE1 = entity1.GenerateSignature(entity1.keyPair.e, entity1.keyPair.d, entity1.keyPair.n)
-# signing the message with entity's private key
+	if Entity(ent).EntityHasCertificate():
+		print("hello")
+		#print(get_keypair_from_file(ent))
 
-# verifying signature with entity's public key
-if VerifySignature(entity1.keyPair.e, sigE1, entity1.keyPair.e, entity1.keyPair.n) == True:
-	entity1.certificate = authority1.GenerateCertificate(entity1.keyPair.e, entity1)
-	# if hashes match, generating certificate for the entity
-
-print("Certificates:")
-entity1.ShowCertificates()
-entity2.ShowCertificates()
-
-VerifyCertificate(entity1.certificate, entity2)
-
-i = 0
-while i<1:
-
-	print("Testing valid file")
-	with open("text.txt", "r") as f:
-		entity1.message = f.read().encode()
-		
-		sigF1 = entity1.GenerateSignature(entity1.message, entity1.keyPair.d, entity1.keyPair.n)
-		# signing the message with entity's private key
-
-		VerifySignature(entity1.message, sigF1, entity1.keyPair.e, entity1.keyPair.n)
-		# verifying signature with entity's public key
-
-	print("Testing altered file")
-	with open("text_altered.txt", "r") as f_altered:
-		altered_message = f_altered.read().encode()
-
-		sigF1 = entity1.GenerateSignature(entity1.message, entity1.keyPair.d, entity1.keyPair.n)
-
-		VerifySignature(altered_message, sigF1, entity1.keyPair.e, entity1.keyPair.n)
+	entity = ent
+	entitySign = "d140111263614299868872663750368351800971083278637884970743158207444012665140553235144935653785840711317036916465673765901092001888475433221014130877911534940683"
+	authority = "CZ_Authority"
+	certificate = "d30921321136354723555129712496783865765041623320981679747660392471935620997016659980413184346423485846515413769506734297"
+	try:
+		sign = {
+			"Entity": entity,
+			"EntitySign": entitySign,
+			"Authority": authority,
+			"Certificate": certificate,
+		}
+		return sign
+	except:
+		return None
 
 
-	print("Signature into metadata for PDF:")
-	sig = entity1.GenerateSignature('doc.pdf', entity1.keyPair.d, entity1.keyPair.n)
-	VerifySignature('doc_signed.pdf', sig, entity1.keyPair.e, entity1.keyPair.n)
+def apkSignFile(file, entName):
+	ent = entDic[entName]
+	keys = ent.keyPair
+	ent.GenerateSignature(file, ent.keyPair.d, ent.keyPair.n)
 
-	print("Signature into metadata for TXT:")
-	txt_signature = entity1.GenerateSignature("signed_text.txt",entity1.keyPair.d, entity1.keyPair.n)
-	VerifySignature("signed_text.txt", txt_signature, entity1.keyPair.e, entity1.keyPair.n)
-	i += 1
+
+def addCertificate(entName, authName):
+	ent = entDic[entName]
+	auth = authDic[authName]
+	ent.certificate = auth.GenerateCertificate(ent.keyPair.e, ent)
+
+
+def create_auth_ent(authority, name):
+	if authority:
+		aut = Authority(name)
+		aut.keyPair = GenerateKeyPair(aut.id, aut.name)
+		authDic[name] = aut
+	else:
+		ent = Entity(name)
+		ent.keyPair = GenerateKeyPair(ent.id, ent.name)
+		entDic[name] = ent
+
+
+entDic = {}
+authDic = {}
+initialize_entities_authorities()
